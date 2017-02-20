15abe033f240b18b0964713ead6bf0b09df6fed6
Konstantinos Leimonis <k.leimonis@gmail.com>
2017-02-20 13:51:31 +0200

    Add react-toolbox and integrate with create-react-app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import theme from '../public/toolbox/theme.js';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+
 import App from './App';
 import './index.css';
+import '../public/toolbox/theme.css';
 
 ReactDOM.render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
   document.getElementById('root')
 );
