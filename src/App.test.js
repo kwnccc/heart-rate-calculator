d0bd5e8711267fa521ecc38bf4fde6c4b52838ad
Konstantinos Leimonis <k.leimonis@gmail.com>
2017-02-23 01:39:28 +0200

    Add App.js tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('should render without crashing', () => {
+  shallow(<App />);
+});
+
+it('should be selectable by class "App"', function() {
+  const wrapper = shallow(<App />);
+  expect(wrapper.is('.App')).toBe(true);
+});
+
+it('should render an AppBar Component', () => {
+  const wrapper = mount(<App />);
+  expect(wrapper.find('AppBar').length).toBe(1);
+});
+
+it('should render a CalculatorContainer Component', () => {
+  const wrapper = mount(<App />);
+  expect(wrapper.find('CalculatorContainer').length).toBe(1);
 });
