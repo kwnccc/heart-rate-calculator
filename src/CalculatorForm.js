d600ca8938f337dbfd0dc11afe1bb1f503257442
Konstantinos Leimonis <k.leimonis@gmail.com>
2017-02-28 17:00:28 +0200

    Add default state for CalculatorForm component

--- src/CalculatorForm.js
+++ src/CalculatorForm.js
@@ -3,14 +3,14 @@ import React from 'react';
 import Button from 'react-toolbox/lib/button/Button';
 import Input from 'react-toolbox/lib/input/Input';
 
-const CalculatorForm = ({age, rhr, error, isButtonEnabled, onChange, onSubmit}) => {
+const CalculatorForm = ({age='', rhr='', error={}, isButtonEnabled=false, onChange, onSubmit}) => {
   const onChangeAge = onChange.bind(this, 'age');
   const onChangeRhr = onChange.bind(this, 'rhr');
   return (
     <div>
-    <Input type='number' label='Age' name='age' value={age} error={error.age} onChange={onChangeAge} maxLength={3} />
-    <Input type='number' label='Resting Heart Rate' name='rhr' value={rhr} error={error.rhr} onChange={onChangeRhr} maxLength={3} />
-    <Button raised primary disabled={!isButtonEnabled} onClick={onSubmit}>Calculate</Button>
+      <Input type='number' label='Age' name='age' value={age} error={error.age} onChange={onChangeAge} maxLength={3} />
+      <Input type='number' label='Resting Heart Rate' name='rhr' value={rhr} error={error.rhr} onChange={onChangeRhr} maxLength={3} />
+      <Button raised primary disabled={!isButtonEnabled} onClick={onSubmit}>Calculate</Button>
     </div>
   );
 }
